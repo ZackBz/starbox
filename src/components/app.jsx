48e17467e2d1af61f6d659121b603da319986fb6
zack <zb01406>
2021-02-12 14:01:33 -0500

    icons

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import {
   f7,
@@ -7,31 +7,15 @@ import {
   Panel,
   Views,
   View,
-  Popup,
   Page,
   Navbar,
   Toolbar,
-  NavRight,
-  Link,
-  Block,
-  BlockTitle,
-  LoginScreen,
-  LoginScreenTitle,
-  List,
-  ListItem,
-  ListInput,
-  ListButton,
-  BlockFooter
+  Link
 } from 'framework7-react';
 
 
 import routes from '../js/routes';
-import store from '../js/store';
-
 const MyApp = () => {
-  // Login screen demo data
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
   // Framework7 Parameters
   const f7params = {
@@ -40,8 +24,6 @@ const MyApp = () => {
 
 
 
-      // App store
-      store: store,
       // App routes
       routes: routes,
       // Register service worker
@@ -49,11 +31,6 @@ const MyApp = () => {
         path: '/service-worker.js',
       },
   };
-  const alertLoginData = () => {
-    f7.dialog.alert('Username: ' + username + '<br>Password: ' + password, () => {
-      f7.loginScreen.close();
-    });
-  }
   f7ready(() => {
 
 
