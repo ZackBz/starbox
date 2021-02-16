03ffc26a236f46ce223cb42a4e46284c13fa2203
zack <zb01406>
2021-02-16 13:51:51 -0500

    plist

--- src/featured.js
+++ src/featured.js
@@ -1,28 +1,28 @@
 export const Featured = 
 [
     {
-    "url": "#",
+    "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/odyssey.plist",
     "image": "https://storage.ihvn.dev/icons/a28e0b49c3fffd8be636472cf7ce540e49235b2520ffac07d4cb9a504bafe288.jpeg",
     "name": "Odyssey Jailbreak",
     "version": "version 1.2.2",
     "subtitle": "JB App"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/tiktok++.plist",
         "image": "https://storage.ihvn.dev/icons/apps/tikitok.png",
         "name": "TikTok++",
         "version": "version 18.0.0",
         "subtitle": "Tweaked Game"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/delta.plist",
         "image": "https://content.app-valley.vip/icon/202.png",
         "name": "Delta",
         "version": "version 1.1.2",
         "subtitle": "Emulator"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/gba4ios.plist",
         "image": "https://storage.ihvn.dev/icons/apps/gba.png",
         "name": "GBA4iOS",
         "version": "version 2.1.1",
