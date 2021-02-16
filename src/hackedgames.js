d750872783b828ce291aa9ef2260961736533291
zack <zb01406>
2021-02-16 09:48:51 -0500

    ads

--- src/hackedgames.js
+++ src/hackedgames.js
@@ -1,7 +1,7 @@
 export const HackedGames = 
 [
     {
-    "url": "#",
+    "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/ipogo.plist",
     "image": "https://storage.ihvn.dev/icons/b4cf627abe479a71e714566b06df1886192c8a350f4962fc45f79601eacbd243.png",
     "name": "iPoGo",
     "version": "version 1.96",
