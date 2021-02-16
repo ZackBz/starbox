fae3c71c9f78e5570420cb99056e8b68c29bfb9e
zack <zb01406>
2021-02-16 13:24:01 -0500

    plist

--- src/apps.js
+++ src/apps.js
@@ -1,47 +1,47 @@
 export const Apps = [{
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/unc0ver.plist",
         "image": "https://storage.ihvn.dev/icons/apps/uncover.png",
         "name": "Unc0ver iOS 11.0 - 13.5",
         "version": "version 1.3.2",
         "subtitle": "Jailbreak App"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/igrocket.plist",
         "image": "https://storage.ihvn.dev/icons/apps/instagram.png",
         "name": "Instagram Rocket",
         "version": "version 1.4.1",
         "subtitle": "Tweaked Instagram"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/spotify.plist",
         "image": "https://storage.ihvn.dev/icons/1609.png",
         "name": "Spotify++",
         "version": "version 4.3.22",
         "subtitle": "Tweaked Spotify"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/minecraft.plist",
         "image": "https://storage.ihvn.dev/icons/apps/minecraft.jpg",
         "name": "Minecraft",
         "version": "version 10.4.1",
         "subtitle": "Minecraft is Free"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/WhatsAppBusiness.plist",
         "image": "https://storage.ihvn.dev/icons/wabusiness.jpg",
         "name": "Watusi WA Business",
         "version": "version 2.20.11",
         "subtitle": "Tweaked App"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/WhatsApp.plist",
         "image": "https://storage.ihvn.dev/icons/apps/whatsapp.png",
         "name": "WhatsApp Watusi",
         "version": "version 2.21.11",
         "subtitle": "Tweaked App"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/Twitterowl.plist",
         "image": "https://storage.ihvn.dev/icons/apps/twitter.png",
         "name": "Twitter Owl",
         "version": "version 8.5.1",
@@ -55,7 +55,7 @@ export const Apps = [{
         "subtitle": "Tweaked App"
     },
     {
-        "url": "#",
+        "url": "itms-services://?action=download-manifest&url=https://storage.starbox.app/signed/Instagram%20Rhino.plist",
         "image": "https://storage.ihvn.dev/icons/b2ce1a321fb3b3ad08abfa1fe7fa85cf532a97d03abf8a1a2712a9de9219081b.png",
         "name": "Instagram Rhino",
         "version": "version 169.0",
